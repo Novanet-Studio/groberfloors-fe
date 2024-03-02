d99e4f4c2eec3dd9ce1b7e4eadd67052b026c6b4
David Arenas <davejs136@gmail.com>
2024-03-02 14:12:59 -0400

    feat: add google-analytics

---
+++ src/common/google-analytics.js
@@ -0,0 +1,9 @@
+export default function initGA(trackingId) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", trackingId);
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,13 @@ import Script from "next/script";
 import Head from "next/head";
 import ProgressScroll from "../components/Progress-Scroll";
 import "../styles/globals.css";
+import initGA from "../common/google-analytics";
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    initGA("G-259FMWPGZF");
+  }, []);
+
   return (
     <>
       <Head>
@@ -38,6 +43,10 @@ function MyApp({ Component, pageProps }) {
         src="/landing-preview/js/parallax.min.js"
       ></Script>
       <Script strategy="afterInteractive" src="/js/metrics.js"></Script>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-259FMWPGZF"
+      ></Script>
       <Script strategy="lazyOnload" src="/js/main.js"></Script>
     </>
   );
